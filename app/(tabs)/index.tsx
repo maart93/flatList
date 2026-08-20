7dd195aeda454e54d30c24d784369a73b4873498
Marilia <marilia.leonardo@alunos.sc.senac.br>
2026-08-20 16:41:04 -0300

    "-"

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,53 +1,53 @@
-import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {View, StyleSheet, FlatList, Text, Image} from 'react-native';
 import Filme from './filme';
 
 const categorias = [
   {
     id: "1",
     titulo: "Em Alta",
-    filmes= [
-      { id: "1a", titulo: "Oppenheimer", cor: "#1a1a2e" },
-      { id: "1b", titulo: "Duna 2", cor: "#16213e" },
-      { id: "1c", titulo: "Barbie", cor: "#0f3460" },
-      { id: "1d", titulo: "Poor Things", cor: "#533483" },
-      { id: "1e", titulo: "Saltburn", cor: "#2b2d42" },
+    filmes: [
+      { id: "1a", titulo: "Oppenheimer", cor: "#1a1a2e", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5"},
+      { id: "1b", titulo: "Duna 2", cor: "#16213e", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5"},
+      { id: "1c", titulo: "Barbie", cor: "#0f3460",image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
+      { id: "1d", titulo: "Poor Things", cor: "#533483", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
+      { id: "1e", titulo: "Saltburn", cor: "#2b2d42", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5"},
     ],
   },
   {
     id: "2",
     titulo: "Ação",
     filmes: [
-      { id: "2a", titulo: "John Wick 4", cor: "#1b1b2f" },
-      { id: "2b", titulo: "Missão Impossível", cor: "#162447" },
-      { id: "2c", titulo: "Top Gun", cor: "#1f4068" },
-      { id: "2d", titulo: "Mad Max", cor: "#1b262c" },
+      { id: "2a", titulo: "John Wick 4", cor: "#1b1b2f", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
+      { id: "2b", titulo: "Missão Impossível", cor: "#162447", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
+      { id: "2c", titulo: "Top Gun", cor: "#1f4068", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
+      { id: "2d", titulo: "Mad Max", cor: "#1b262c", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
     ],
   },
   {
     id: "3",
     titulo: "Comédia",
     filmes: [
-      { id: "3a", titulo: "Superbad", cor: "#2d132c" },
-      { id: "3b", titulo: "The Grand Budapest", cor: "#1c3334" },
-      { id: "3c", titulo: "Knives Out", cor: "#2c003e" },
+      { id: "3a", titulo: "Superbad", cor: "#2d132c", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
+      { id: "3b", titulo: "The Grand Budapest", cor: "#1c3334", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
+      { id: "3c", titulo: "Knives Out", cor: "#2c003e", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
     ],
   },
   {
     id: "4",
     titulo: "Documentários",
     filmes: [
-      { id: "4a", titulo: "Free Solo", cor: "#0d0d0d" },
-      { id: "4b", titulo: "The Social Dilemma", cor: "#001011" },
-      { id: "4c", titulo: "My Octopus Teacher", cor: "#002b36" },
+      { id: "4a", titulo: "Free Solo", cor: "#0d0d0d", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
+      { id: "4b", titulo: "The Social Dilemma", cor: "#001011", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5"},
+      { id: "4c", titulo: "My Octopus Teacher", cor: "#002b36", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5" },
     ],
   },
   {
     id: "5",
     titulo: "Terror",
     filmes: [
-      { id: "5a", titulo: "Hereditary", cor: "#200122" },
-      { id: "5b", titulo: "Midsommar", cor: "#190a05" },
-      { id: "5c", titulo: "Get Out", cor: "#0a0a0a" },
+      { id: "5a", titulo: "Hereditary", cor: "#200122", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5"},
+      { id: "5b", titulo: "Midsommar", cor: "#190a05", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5"},
+      { id: "5c", titulo: "Get Out", cor: "#0a0a0a", image:"https://www.bing.com/th?id=OPHS.1Aylece0yiDQ7w474C474&o=5&pid=21.1&w=140&h=163&qlt=100&dpr=1&o=2&pcl=f5f5f5"},
     ],
   },
 ];
@@ -66,25 +66,38 @@ export default function App() {
     );
 }
 
-function renderCategoria((item):(item:any)){
+function renderCategoria ((item):(item:any)){
   return(
     <View>
       {item.titulo}
       <FlatList>
-        data={filmes}
-        keyExtractor={{item} => filme.id}
+        data={item.filmes}
+        keyExtractor={{item} => filmes.id}
         horizontal={true}
         renderItem= {filme}
       </FlatList>
     </View>
   )
 }
-function filme((item):(item:any)){
-  <View>
-  {{item} => <Text style={nomeFilme}>{item.nome}</Text>}
-</View>
+function filme ((item):(item:any)){
+  return(
+    <View>
+      <Text style={nomeFilme}>{item.titulo}</Text>
+      <Image
+        source={{uri:item.image}}
+        style={[styles.imagem]}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-       
+  nomeFilme:{
+    backgroundColor: "#559680",
+  },
+  imagem: {
+    width: 150,
+    height: 200,
+    margin: 10
+  },
 });
